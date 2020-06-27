92df11572169efa9c494074b355cf93ed4173647
hakokoha <kriskosi123@gmail.com>
2020-06-27 14:32:04 +0300

    added express server to handle backed stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51Gt93LKgdL7yCyvUtMQkHrtWMhLSVWuVolmsBazppDabvZk9rWS0qPaORNKiHs9c0utyRJohdIKmO28V0igC0TbL007bLIV7zw";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Succesful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please make sure the provided credit card"
+        );
+      });
   };
 
   return (
